198c567e121295f4386f17827ad36a07a8d43854
Venmis <nmisiukonis430@gmail.com>
2021-04-05 23:20:44 +0100

    Basic feature extraction with rolling window on time series analysis

--- ECG_data_process.py
+++ ECG_data_process.py
@@ -12,8 +12,19 @@ dataframe[1] = [series.index[i] for i in range(len(series))]
 print(dataframe)
 #Time series analysis - Lag features
 
+temps = dataframe
+width = 1000
+shifted = temps.shift(width - 1)
+window = shifted.rolling(window=width)
 
-dataframe_lagged = concat([temp.shift(1), temp], axis=1)
-print(dataframe_lagged)
-dataframe_lagged.columns = ['t-1', 't+1']
-print(dataframe_lagged.head(5))
+dataframe = concat([window.min(), window.mean(), window.max(), temps], axis=1)
+dataframe.columns = ['min', 'mean', 'max', 't+1']
+print(dataframe.head(10000))
+
+dataframe.to_csv('ECG_features', index=False)
+		
+		
+#dataframe_lagged = concat([temp.shift(1), temp], axis=1)
+#print(dataframe_lagged)
+#dataframe_lagged.columns = ['t-1', 't+1']
+#print(dataframe_lagged.head(5))
