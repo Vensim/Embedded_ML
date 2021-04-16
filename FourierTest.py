f3fa108d9fe6ce8c7ab3b4c982443322a39bf0b5
Venmis <nmisiukonis430@gmail.com>
2021-04-16 14:19:14 +0100

    Fourier Transform init

---
+++ FourierTest.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+t = np.linspace(0, 0.5, 500)
+s = np.sin(40 * 2 * np.pi * t) + 0.5 * np.sin(90 * 2 * np.pi * t)
+
+plt.ylabel("Amplitude")
+plt.xlabel("Time [s]")
+plt.plot(t, s)
+plt.show()
+
+fft = np.fft.fft(s)
+T = t[1] - t[0]  # sampling interval 
+N = s.size
+
+# 1/T = frequency
+f = np.linspace(0, 1 / T, N)
+
+plt.ylabel("Amplitude")
+plt.xlabel("Frequency [Hz]")
+plt.bar(f[:N // 2], np.abs(fft)[:N // 2] * 1 / N, width=1.5)  # 1 / N is a normalization factor
+plt.show()
--- WindowingTest.py
+++ WindowingTest.py
@@ -1,4 +1,3 @@
-
 #Libraries
 import pandas as pd
 from pandas import read_csv
@@ -7,24 +6,25 @@ from pandas import concat
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 #Initialisations
-series = read_csv('AD8232_data.csv', header=0, index_col=0).T
+series = read_csv('AD8232_data_base.csv', header=0, index_col=0).T
 print(series)
 dataframe = DataFrame()
 dataframe[1] = [series.index[i] for i in range(len(series))]
 print(dataframe)
 
+
+
 #Time series analysis - Lag features
+t = np.arange(0, 5000, 1)
+
 temps = dataframe
 width = 1000
 shifted = temps.shift(width - 1)
 window = shifted.rolling(window=width)
-
-dataframe = concat([window.min(), window.mean(), window.max(), temps], axis=1)
-dataframe.columns = ['min', 'mean', 'max', 'sensor']
-print(dataframe.head(10000))
-
-dataframe.to_csv('ECG_features.csv', index=False)
-
-
+print("Data Frame:")
+print(temps)
+print("Shifted frame: ")
+print(shifted)
+print("Window : ")
+print(window)
