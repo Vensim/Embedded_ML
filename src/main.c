6f9fc5122495aaabe912584c089e35ec1ae2947f
Vensim1 <nmisiukonis430@gmail.com>
2022-06-19 23:02:57 +0100

    Re-adjustment of source files

--- src/main.c
+++ src/main.c
@@ -1,5 +1,10 @@
-#include <stdio.h>
 
+#include <stdio.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_spi_flash.h"
 void app_main(void)
 {
   printf("Hello world!\n");
